9c51b89501859c7004ec9c7532b89264b1d9091c
Dimural Murat <dimural722@gmail.com>
2025-11-21 22:19:50 -0500

    entity and repository done

--- src/main/java/com/expensetracker/expensetracker/category/Category.java
+++ src/main/java/com/expensetracker/expensetracker/category/Category.java
@@ -1,23 +1,82 @@
 package com.expensetracker.expensetracker.category;
 
+import java.time.Instant;
 import java.util.UUID;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import com.expensetracker.expensetracker.user.User;
+import jakarta.persistence.Column;
 
 @Entity
 public class Category {
 
     @Id
-    UUID id = UUID.randomUUID();
+    @GeneratedValue
+    private UUID id;
 
-    String name;
-    CategoryTypes type;
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private CategoryTypes type;
 
     @ManyToOne
-    User user;  //user entity is not made yet, that is why there is an error my fellow bakaaaaaaa
+    @JoinColumn(name = "user_id")
+    private User user;  
     
-    String createdAt = "now";
+    @Column(nullable = false)
+    private Instant createdAt = Instant.now();
+
+
+    public Category() {
+        
+    }
+
+    public Category(String name, CategoryTypes type, User user) {
+        this.name = name;
+        this.type = type;
+        this.user = user;
+        this.createdAt = Instant.now();
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setType(CategoryTypes type) {
+        this.type = type;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public CategoryTypes getType() {
+        return type;
+    }
+    public User getUser() {
+        return user;
+    }
+
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Instant createdAt) {
+        this.createdAt = createdAt;
+    }
 }
--- src/main/java/com/expensetracker/expensetracker/category/CategoryRepository.java
+++ src/main/java/com/expensetracker/expensetracker/category/CategoryRepository.java
@@ -1,5 +1,17 @@
 package com.expensetracker.expensetracker.category;
 
-public class CategoryRepository {
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.expensetracker.expensetracker.user.User;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CategoryRepository extends JpaRepository<Category, UUID> {
+
+    List<Category> findByUser(User user);
+
+    Optional<Category> findByUserAndName(User user, String name);
     
-}
+} 
