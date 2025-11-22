4915d3cc86ba7a97772369de06f24125dee60ca4
Dimural Murat <dimural722@gmail.com>
2025-11-22 00:20:21 -0500

    category part done (except for some logic which requires the authentication)

--- src/main/java/com/expensetracker/expensetracker/category/CategoryController.java
+++ src/main/java/com/expensetracker/expensetracker/category/CategoryController.java
@@ -1,5 +1,53 @@
 package com.expensetracker.expensetracker.category;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.expensetracker.expensetracker.category.dto.CategoryResponseDto;
+import com.expensetracker.expensetracker.category.dto.CreateCategoryDto;
+import com.expensetracker.expensetracker.user.User;
+import com.expensetracker.expensetracker.user.UserRepository;
+
+@RestController
+@RequestMapping("/api/categories")
 public class CategoryController {
+    private final CategoryService categoryService;
+    private final UserRepository userRepository;
+
+    public CategoryController(CategoryService categoryService, UserRepository userRepository) {
+        this.categoryService = categoryService;
+        this.userRepository = userRepository;
+    }
     
+    private User getCurrentUser() {
+
+        return userRepository.findAll().stream()
+                .findFirst()
+                .orElseThrow(() -> new RuntimeException("No user found in database"));
+
+       //this cannot be made yet, it needs the authentication stuff and blah blah blah. it is supposesd to get the current logged in user
+       // I just made it get first user, THIS CODE IS INCORRECT BUT TEMPORARY DO NOT FORGET HAHAHAHAAAHAHAHAHAHAHA
+    }
+
+    @PostMapping
+    public CategoryResponseDto create(@RequestBody CreateCategoryDto dto) {
+        User user = getCurrentUser();
+        return categoryService.createCategory(user, dto);
+
+        //IMPORTANT MUST READ ************* this cannot work as planned bc the getCurrentUser is not made yet.
+        //However the above code is correct and is supposed to work once the authentication stuff is done
+    }
+
+    public List<CategoryResponseDto> list() {
+        User user = getCurrentUser();
+        return categoryService.getCategoriesForUser(user);
+
+        //IMPORTANT MUST READ ************* same thing as in the other method.
+        //The code above is correct and is supposed to work once the authentication stuff is done
+    }
+
 }
--- src/main/java/com/expensetracker/expensetracker/category/CategoryService.java
+++ src/main/java/com/expensetracker/expensetracker/category/CategoryService.java
@@ -1,5 +1,44 @@
 package com.expensetracker.expensetracker.category;
 
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.expensetracker.expensetracker.category.dto.CategoryResponseDto;
+import com.expensetracker.expensetracker.category.dto.CreateCategoryDto;
+import com.expensetracker.expensetracker.user.User;
+
+@Service
 public class CategoryService {
+    private final CategoryRepository categoryRepository;
     
+    public CategoryService(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    public CategoryResponseDto createCategory(User user, CreateCategoryDto dto) {
+        categoryRepository.findByUserAndName(user, dto.name())
+            .ifPresent(existingCategory -> {
+                throw new IllegalArgumentException("Category with the same name already exists for this user.");
+            });
+        Category category = new Category(dto.name(), dto.type(), user);
+        Category savedCategory = categoryRepository.save(category);
+        return toResponseDto(savedCategory);
+    }
+
+    public CategoryResponseDto toResponseDto(Category category) {
+        return new CategoryResponseDto(
+            category.getId(),
+            category.getName(),
+            category.getType(),
+            category.getCreatedAt()
+        );
+    }
+
+    public List<CategoryResponseDto> getCategoriesForUser(User user) {
+        List<Category> categories = categoryRepository.findByUser(user);
+        return categories.stream()
+                .map(this::toResponseDto)
+                .toList();
+    }
 }
