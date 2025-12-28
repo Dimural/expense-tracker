1f43b8d257979cf92d90923a09fa313524710b08
Dimural Murat <dimural722@gmail.com>
2025-12-27 19:41:04 -0500

    transaction entity done

--- src/main/java/com/expensetracker/expensetracker/transaction/Transaction.java
+++ src/main/java/com/expensetracker/expensetracker/transaction/Transaction.java
@@ -1,8 +1,45 @@
 package com.expensetracker.expensetracker.transaction;
 
+import java.math.BigDecimal;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.util.UUID;
+
+import com.expensetracker.expensetracker.user.User;
+
+import java.util.Locale.Category;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.criteria.CriteriaBuilder.In;
 
 @Entity
 public class Transaction {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @Column(nullable = false, precision = 19, scale = 2)
+    private BigDecimal amount;
+
+    @Column(length = 500)
+    private String description;
+
+    @Column(nullable = false)
+    private LocalDate date;
+
+    @ManyToOne @JoinColumn(name = "category_id", nullable = false)
+    private Category category;
+
+    @ManyToOne @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(nullable = false)
+    private Instant createdAt = Instant.now();
     
 }
