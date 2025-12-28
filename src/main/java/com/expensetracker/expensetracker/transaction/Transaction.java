587094a765c96136af0d2ec81a429d227e3dd77b
Dimural Murat <dimural722@gmail.com>
2025-12-28 17:50:06 -0500

    data access created

--- src/main/java/com/expensetracker/expensetracker/transaction/Transaction.java
+++ src/main/java/com/expensetracker/expensetracker/transaction/Transaction.java
@@ -15,7 +15,6 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.criteria.CriteriaBuilder.In;
 
 @Entity
 public class Transaction {
@@ -41,5 +40,58 @@ public class Transaction {
 
     @Column(nullable = false)
     private Instant createdAt = Instant.now();
-    
+
+    public Transaction() {}
+
+    public Transaction(BigDecimal amount, String description, LocalDate date, Category category, User user) {
+        this.amount = amount;
+        this.description = description;
+        this.date = date;
+        this.category = category;
+        this.user = user;
+        this.createdAt = Instant.now();
+    }
+    public UUID getId() {
+        return id;
+    }
+    public BigDecimal getAmount() {
+        return amount;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public LocalDate getDate() {
+        return date;
+    }
+    public Category getCategory() {
+        return category;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+    public void setDescription(String description) {
+        this.description = description; 
+    }
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+    public void setCreatedAt(Instant createdAt) {
+        this.createdAt = createdAt;
+    }
+
 }
